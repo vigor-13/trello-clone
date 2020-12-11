5a826cbb0506a3812209bdf054976ce111b9fcfd
Decoy5796 <kkw5796@gmail.com>
2020-12-11 09:53:52 +0900

    feat: add Board component

--- trello-vue/src/router/index.js
+++ trello-vue/src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Home from '../components/Home.vue';
-import Login from '../components/Login.vue';
-import NotFound from '../components/NotFound.vue';
+import Home from '../components/Home';
+import Login from '../components/Login';
+import Board from '../components/Board';
+import NotFound from '../components/NotFound';
 
 Vue.use(VueRouter);
 
@@ -12,6 +13,7 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/login', component: Login },
+    { path: '/b/:bid', component: Board },
     { path: '*', component: NotFound },
   ]
 });
