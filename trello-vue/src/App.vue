8713ff3ebfc6f413e3f15675a6e1777fab5857cb
Decoy5796 <kkw5796@gmail.com>
2020-12-11 09:33:02 +0900

    feat: add vue-router

--- trello-vue/src/App.vue
+++ trello-vue/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ trello-vue/src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    404: Page not found.
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
