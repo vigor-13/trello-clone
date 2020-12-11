532607afb34fc57b2b7a4543276bbcf442d20598
Decoy5796 <kkw5796@gmail.com>
2020-12-11 20:41:00 +0900

    feat: navigation guard

--- trello-vue/src/router/index.js
+++ trello-vue/src/router/index.js
@@ -9,15 +9,40 @@ import NotFound from '../components/NotFound';
 
 Vue.use(VueRouter);
 
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem('token');
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`;
+  isAuth ? next() : next(loginPath);
+};
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
-    { path: '/login', component: Login },
-    { path: '/b/:bid', component: Board, children: [
-      { path: 'c/:cid', component: Card }
-    ] },
-    { path: '*', component: NotFound },
+    {
+      path: '/',
+      component: Home,
+      beforeEnter: requireAuth
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/b/:bid',
+      component: Board,
+      beforeEnter: requireAuth,
+      children: [
+        {
+          path: 'c/:cid',
+          component: Card,
+          beforeEnter: requireAuth
+        }
+      ]
+    },
+    {
+      path: '*',
+      component: NotFound
+    },
   ]
 });
 
